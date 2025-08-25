cc8ab302bc1b6e0da6ac4eae4f8d1bc827d53718
KimBaaaaap <amarkim43@gmail.com>
2025-08-25 17:01:18 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+app.use(express.json());
+
+const FILE = "dogs.json";
+
+
+let dogs = fs.existsSync(FILE) ? JSON.parse(fs.readFileSync(FILE)) : [];
+let nextId = dogs.reduce((max, d) => Math.max(max, d.id), 0) + 1;
+
+const save = () => fs.writeFileSync(FILE, JSON.stringify(dogs, null, 2));
+
+
+
+
+app.post("/dogs", (req, res) => {
+  const dog = { id: nextId++, ...req.body };
+  dogs.push(dog);
+  save();
+  res.status(201).json(dog);
+});
+
+
+
+app.get("/dogs", (req, res) => res.json(dogs));
+
+app.get("/dogs/:id", (req, res) => {
+  const dog = dogs.find(d => d.id == req.params.id);
+  dog ? res.json(dog) : res.status(404).json({ message: "Dog not found" });
+});
+
+
+
+app.put("/dogs/:id", (req, res) => {
+  const dog = dogs.find(d => d.id == req.params.id);
+  if (!dog) return res.status(404).json({ message: "Dog not found" });
+  Object.assign(dog, req.body);
+  save();
+  res.json(dog);
+});
+
+
+
+app.delete("/dogs/:id", (req, res) => {
+  const i = dogs.findIndex(d => d.id == req.params.id);
+  if (i < 0) return res.status(404).json({ message: "Dog not found" });
+  const [deleted] = dogs.splice(i, 1);
+  save();
+  res.json(deleted);
+});
+
+
+app.listen(3000, () => console.log("http://localhost:3000"));
+
+
+
